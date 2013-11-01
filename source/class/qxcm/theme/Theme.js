0b2ab7c0bda9fcb145ed83cd0a3713e9437c1535
Greg Hellings <greg.hellings@gmail.com>
2013-10-31 22:48:05 -0500

    First checkin.

---
+++ source/class/qxcm/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qxcm.theme.Theme",
+{
+  meta :
+  {
+    color : qxcm.theme.Color,
+    decoration : qxcm.theme.Decoration,
+    font : qxcm.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : qxcm.theme.Appearance
+  }
+});
